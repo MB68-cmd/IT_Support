04007a7e593da84d089f72c0f678d4d48bf51f05
MB68-cmd <bilandzijamarin68@gmail.com>
2026-03-05 13:43:55 +0100

    mit der Schleife begonnen

--- IT_Support/Program.cs
+++ IT_Support/Program.cs
@@ -45,7 +45,7 @@
 
             //SPIEL STARTEN
 
-            bool spielLäuft = true;
+            bool spielLaeuft = true;
             Raum aktuellerRaum = flur;
 
 
@@ -58,20 +58,33 @@
 
             // SPIEL SCHLEIFE
 
-            while (spielLäuft)  
+            // SPIEL-SCHLEIFE
+            while (spielLaeuft)
             {
-                Console.WriteLine("===================");
-                Console.WriteLine($"Du bist im {flur.Name}.");
-                Console.WriteLine(flur.Beschreibung);
-                Console.WriteLine("Ausgänge:");
-                foreach (var ausgang in flur.Ausgaenge)
+                Console.WriteLine("\n-----------------------------------");
+                Console.WriteLine($"Du bist hier: {aktuellerRaum.Name}");
+                Console.WriteLine(aktuellerRaum.Beschreibung);
+
+
+
+                // Liste für das Menü der verfügbaren Ausgänge
+                var wege = new List<string>(aktuellerRaum.Ausgaenge.Keys);    // Hier werden die verfügbaren Ausgänge des aktuellen Raums in eine Liste umgewandelt, damit sie im Menü angezeigt werden können.
+                var ding = aktuellerRaum.GegenstaendeImRaum;                  // Hier wird die Liste der Gegenstände im aktuellen Raum in eine Variable gespeichert, um sie später anzuzeigen.
+
+                Console.WriteLine("wohin willst du?");
+
+                // Menü für die verfügbaren Ausgänge
+
+                for (int i = 0; i < wege.Count; i++)
                 {
-                    Console.WriteLine($"- {ausgang.Key} führt zum {ausgang.Value.Name}");
+                    Raum ziel = aktuellerRaum.Ausgaenge[wege[i]];   // Hier wird der Zielraum für den aktuellen Ausgang aus dem Dictionary der Ausgänge des aktuellen Raums abgerufen.
+                    Console.WriteLine($"{i + 1}. {wege[i]} -> {ziel.Name}");   // Hier wird die Nummer, die Richtung und der Name des Zielraums für jeden verfügbaren Ausgang angezeigt.
+                    Console.ReadLine(); 
                 }
 
-                
-            }
 
+
+            }
         }
     }
 }
